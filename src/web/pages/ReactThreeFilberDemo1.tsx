02f649c4c177b8f721c3fa991b3cdfef7dd427ef
cutefcc <cutefcc@126.com>
2022-06-29 20:21:24 +0800

    feat: react-three-fiber test

--- src/web/pages/ReactThreeFilberDemo1.tsx
+++ src/web/pages/ReactThreeFilberDemo1.tsx
@@ -1,16 +1,25 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
 function App() {
+  const Floor = () => (
+    <mesh position={[0, 0, 0]}>
+      <planeGeometry attach="geometry" args={[10, 10]} />
+      <meshStandardMaterial attach="material" color="blue" />
+    </mesh>
+  );
   return (
     <Canvas>
+      <PerspectiveCamera makeDefault position={[-2, 5, 6]} />
       <OrbitControls />
       <ambientLight />
-      <pointLight position={[10, 10, 10]} />
+      {/* <pointLight position={[10, 10, 10]} /> */}
+      <directionalLight position={[10, 10, 10]} intensity={0.9} />
       <mesh>
         <boxGeometry attach="geometry" args={[1, 1, 1]} />
         <meshStandardMaterial attach="material" color="hotpink" />
       </mesh>
+      <Floor />
     </Canvas>
   );
 }
