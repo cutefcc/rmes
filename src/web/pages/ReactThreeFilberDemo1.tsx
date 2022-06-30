17c84f515f43ff259cf4b77b6cc5de27b9924748
cutefcc <cutefcc@126.com>
2022-06-30 19:53:02 +0800

    feat: react-three-fiber shadow

--- src/web/pages/ReactThreeFilberDemo1.tsx
+++ src/web/pages/ReactThreeFilberDemo1.tsx
@@ -3,19 +3,19 @@ import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 
 function App() {
   const Floor = () => (
-    <mesh position={[0, 0, 0]}>
+    <mesh position={[0, -0.5, 0]} rotation={[-Math.PI / 2, 0, 0]} receiveShadow>
       <planeGeometry attach="geometry" args={[10, 10]} />
       <meshStandardMaterial attach="material" color="blue" />
     </mesh>
   );
   return (
-    <Canvas>
-      <PerspectiveCamera makeDefault position={[-2, 5, 6]} />
+    <Canvas shadows={true}>
+      <PerspectiveCamera makeDefault position={[-2, 3, 8]} />
       <OrbitControls />
       <ambientLight />
       {/* <pointLight position={[10, 10, 10]} /> */}
-      <directionalLight position={[10, 10, 10]} intensity={0.9} />
-      <mesh>
+      <directionalLight position={[10, 10, 10]} intensity={0.9} castShadow />
+      <mesh castShadow>
         <boxGeometry attach="geometry" args={[1, 1, 1]} />
         <meshStandardMaterial attach="material" color="hotpink" />
       </mesh>
