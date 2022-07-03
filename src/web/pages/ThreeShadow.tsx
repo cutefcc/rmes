4d9a34e45e4344944ecb7e447ebd1c80eb83e7ab
cutefcc <cutefcc@126.com>
2022-07-03 21:58:35 +0800

    feat: threejs shadow

--- src/web/pages/ThreeShadow.tsx
+++ src/web/pages/ThreeShadow.tsx
@@ -3,6 +3,7 @@ import { Mesh } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import gsap from 'gsap';
 import { memo, useEffect } from 'react';
+import { LightConstants } from '@babylonjs/core/Lights/lightConstants';
 
 // 相机： 视角、宽高比、近裁剪面、远裁剪面
 // fov — Camera frustum vertical field of view. Default value is 50.
@@ -23,31 +24,35 @@ const createLight = scene => {
   light.shadow.mapSize.height = 512;
   light.shadow.camera.near = 0.5;
   light.shadow.camera.far = 100;
+  light.position.z = 10;
+  light.position.y = 5;
+  // 添加灯光到场景
   scene.add(light);
 
-  //const helper = new THREE.DirectionalLightHelper(light);
-  const helper = new THREE.CameraHelper(light.shadow.camera);
+  // 灯光helper
+  const helper = new THREE.DirectionalLightHelper(light);
+  // camerraHeaper
+  const cameraHelper = new THREE.CameraHelper(light.shadow.camera);
   scene.add(helper);
+  scene.add(cameraHelper);
 };
 
 const createGround = scene => {
-  const planeGeometry = new THREE.PlaneGeometry(100, 20);
+  const planeGeometry = new THREE.PlaneGeometry(10, 10);
   const plane = new THREE.Mesh(planeGeometry, new THREE.MeshPhongMaterial());
   plane.rotateX(-Math.PI / 2);
-  plane.position.y = -1.75;
+  plane.position.y = 0;
   plane.receiveShadow = true;
   scene.add(plane);
 };
 
 const addBox = scene => {
   const geometry = new THREE.BoxGeometry(1, 1, 1);
-  // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
   const material = new THREE.MeshStandardMaterial({ color: 0x00ff00 });
   const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
   cube.castShadow = true; //  消费投影
   cube.receiveShadow = true; //default
-  cube.position.y = 3.5;
+  cube.position.y = 0.5;
   gsap.to(cube.rotation, {
     duration: 3,
     y: Math.PI * 2,
@@ -55,35 +60,35 @@ const addBox = scene => {
     yoyo: true,
     ease: 'linear',
   });
-
-  return cube;
+  scene.add(cube);
 };
 
 function ThreeShadow() {
   useEffect(() => {
     const canvas = document.querySelector<HTMLCanvasElement>('#canvas')!;
+    // 相机
     const camera = new THREE.PerspectiveCamera(
       75,
       window.innerWidth / window.innerHeight,
       0.1,
       1000
     );
-    camera.position.z = 7;
 
     const renderer = new THREE.WebGLRenderer({ canvas, antialias: true }); // antialias 扛锯齿
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    // renderer.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+    // 场景
     const scene = new THREE.Scene();
+
     scene.add(camera);
-    // document.body.appendChild(renderer.domElement);
     const controls = new OrbitControls(camera, renderer.domElement);
     camera.position.z = 3;
-
+    // 将灯光添加到场景
     createLight(scene);
-
+    // 添加地面
     createGround(scene);
+    // 添加立方体
     addBox(scene);
     // 理解为轨道相机
     controls.update();
