10fc91b390f2257072860208b68a963cc8c9ff6c
cutefcc <cutefcc@126.com>
2022-07-05 22:56:51 +0800

    feat: adjust folder

--- src/web/layouts/MainLayout.tsx
+++ src/web/layouts/MainLayout.tsx
@@ -1,10 +1,43 @@
-import Menu from '@components/Menu/Menu';
+import { FC, useState } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import MenuConfig from '@constants/MenuConfig';
+import { MenuRouteConfig } from '@constants/MenuRouteConfig';
+import './index.css';
 
-const MainLayout = (): JSX.Element => {
+const { Header, Content, Sider } = Layout;
+
+const MainLayout: FC = (): JSX.Element => {
+  // Sider 收起状态，初始化不收起
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
   return (
-    <div>
-      <Menu />
-    </div>
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+        <div className="logo" style={{ height: '64px' }}>
+          cutefcc playground
+        </div>
+        <div className="menu">
+          <Menu
+            onClick={e => {
+              if (MenuRouteConfig.hasOwnProperty(e.key)) {
+                navigate(MenuRouteConfig[e.key]);
+              }
+            }}
+            theme="dark"
+            defaultSelectedKeys={['1']}
+            mode="inline"
+            items={MenuConfig()}
+          />
+        </div>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
   );
 };
 export default MainLayout;
