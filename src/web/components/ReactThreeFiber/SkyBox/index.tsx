de4cc0fa226e5ce3ec22e315c0d54305adc1dbcd
cutefcc <cutefcc@126.com>
2022-07-06 09:06:35 +0800

    feat: spaceshif skybox

---
+++ src/web/components/ReactThreeFiber/SkyBox/index.tsx
@@ -0,0 +1,30 @@
+import { useTexture } from '@react-three/drei';
+import * as THREE from 'three';
+import { ModelsHost } from '@constants/ModelsConfig';
+import { useLayoutEffect } from 'react';
+const SkyBox = () => {
+  // 加载textures
+  const texture = useTexture(`${ModelsHost}textures/galaxy.jpg`);
+
+  useLayoutEffect(() => {
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(1.9, 1.9);
+  }, [texture]);
+
+  return (
+    <>
+      <mesh>
+        <sphereGeometry attach="geometry" args={[100, 100, 100]} />
+        {/* 带自发光的天空盒 */}
+        <meshPhongMaterial
+          attach="material"
+          emissive={new THREE.Color(0xff2190)}
+          emissiveIntensity={0.1}
+          side={THREE.BackSide}
+          map={texture}
+        />
+      </mesh>
+    </>
+  );
+};
+export default SkyBox;
--- src/web/pages/reactThreeFiber/Spaceship.tsx
+++ src/web/pages/reactThreeFiber/Spaceship.tsx
@@ -2,6 +2,7 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera, Html, useProgress, useGLTF } from '@react-three/drei';
 import { Physics, usePlane, useBox } from '@react-three/cannon';
 import { Suspense } from 'react';
+import SkyBox from '@components/ReactThreeFiber/SkyBox';
 
 // 涉及到shadow cannon 循环组件 click 事件[原生three绑定事件很麻烦，在react-three中没有这个问题]
 
@@ -12,6 +13,8 @@ function App() {
       <OrbitControls />
       <ambientLight />
       <directionalLight position={[10, 10, 10]} intensity={0.9} castShadow />
+      {/* <Physics></Physics> */}
+      <SkyBox />
     </Canvas>
   );
 }
