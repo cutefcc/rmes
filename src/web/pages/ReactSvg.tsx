64a8ddeb55dd1984de17ae1c5f4db582aebd4185
cutefcc <cutefcc@126.com>
2022-07-07 09:58:57 +0800

    fix: react-svg info

--- src/web/pages/ReactSvg.tsx
+++ src/web/pages/ReactSvg.tsx
@@ -5,6 +5,7 @@ const ReactSvg = () => {
       <div>react-svg</div>
       <div>SVG 文件可通过以下标签嵌入 HTML 文档：embed、object 或者 iframe。</div>
       <div>react-svg是一个react组件，便于我们将 SVG 注入 DOM 的 React 组件</div>
+      <div>react-svg是一个交小众的一个库，我关注到时，started：668</div>
       <div>demo:</div>
       <div className="flex">
         <ReactSVG src="http://www.cutefcc.com:81/svg/evil-halloween-hand-svgrepo-com.svg" />
