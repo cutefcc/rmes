54fa9c2d02e2f2b30f7fdd4b361d7b73f8694100
cutefcc <cutefcc@126.com>
2022-09-03 20:43:10 +0800

    feat: Unshift

--- src/web/type_challenges/12.Push.ts
+++ src/web/type_challenges/12.Push.ts
@@ -18,7 +18,7 @@
 
 /* _____________ 你的代码 _____________ */
 
-type Push<T, U> = T extends unknown[] ? [...T, U] : false;
+type Push<T, U> = T extends unknown[] ? [...T, U] : never;
 
 /* _____________ 测试用例 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils';
