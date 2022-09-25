710976d8903b4694b246e6f7199893475353a6dc
cutefcc <cutefcc@126.com>
2022-09-25 22:32:45 +0800

    feat: store

--- src/web/connector/store.ts
+++ src/web/connector/store.ts
@@ -2,10 +2,29 @@ import create from 'zustand/vanilla';
 import { immer } from 'zustand/middleware/immer';
 import { Web3ReactState, Web3ReactStore, Web3ReactActions, Web3ReactStateUpdate } from './types';
 import { utils } from 'ethers';
+import { isEqual } from 'lodash-es';
 
 const { getAddress } = utils;
 
 export const MAX_SAFE_CHAIN_ID = 4503599627370476; //floor((2 ** 53 - 39) / 2);
+export class ChainIdNotAllowError extends Error {
+  public readonly chainId: number;
+
+  public constructor(chainId: number, allowedChainIds: number[]) {
+    super('this network is not supported');
+    this.chainId = chainId;
+    this.name = ChainIdNotAllowError.name;
+    Object.setPrototypeOf(this, ChainIdNotAllowError.prototype);
+  }
+}
+function ensureChainIdIsAllowed(
+  chainId: number,
+  allowedChainIds: number[]
+): ChainIdNotAllowError | undefined {
+  return allowedChainIds.some(allowedChainId => allowedChainId === chainId)
+    ? undefined
+    : new ChainIdNotAllowError(chainId, allowedChainIds);
+}
 function validateChainId(chainId: number) {
   if (!Number.isInteger(chainId) || chainId < 0 || chainId > MAX_SAFE_CHAIN_ID) {
     throw new Error(`Invalid chainId: ${chainId}`);
@@ -24,7 +43,9 @@ const DEFAULT_STORE = {
 
 // https://github.com/Uniswap/web3-react/blob/main/packages/store/src/index.ts
 // return [Web3ReactStore, Web3ReactActions] -> web3-react-hoooks 钱包的具体操作
-export function createWeb3ReactStoreAndActions(): [Web3ReactStore, Web3ReactActions] {
+export function createWeb3ReactStoreAndActions(
+  allowedChainIds: number[]
+): [Web3ReactStore, Web3ReactActions] {
   // 引入zustand/middleware/immer 解决 Re-render 问题
   const store = create(immer<Web3ReactState>(() => DEFAULT_STORE));
   const { getState, setState, subscribe, destroy } = store;
@@ -85,13 +106,31 @@ export function createWeb3ReactStoreAndActions(): [Web3ReactStore, Web3ReactActi
     setState(draft => {
       const chainId = stateUpdate.chainId ?? draft.chainId;
       const accounts = stateUpdate.accounts ?? draft.accounts;
-      const error = draft.error;
-      if (chainId) {
+      let active = draft.active;
+      let error = draft.error;
+      if (chainId && allowedChainIds) {
+        const chainIdError = ensureChainIdIsAllowed(chainId, allowedChainIds);
+        if (chainIdError && error) {
+          if (chainIdError instanceof ChainIdNotAllowError) {
+            console.log(`${chainIdError.name} is being clobbered by ${chainIdError.name}`);
+          }
+        }
+        error = chainIdError;
       }
+
       if (error) {
       }
-      // if (active) {
-      // }
+      if (active && (error || (chainId && accounts))) {
+        active = false;
+      }
+      draft.chainId = chainId;
+      // avoid re-render
+      if (!isEqual(draft.accounts, accounts)) {
+        draft.accounts = accounts;
+      }
+      draft.accounts = accounts;
+      draft.error = error;
+      draft.active = active;
     });
   }
   function reportError(error: Error | undefined): void {
