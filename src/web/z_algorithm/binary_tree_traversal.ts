88078c3b4000890c39d9e9e19c8f29cc458d7f06
cutefcc <cutefcc@126.com>
2022-10-15 22:33:10 +0800

    feat: tree_pre_back_inorder

--- src/web/z_algorithm/binary_tree_traversal.ts
+++ src/web/z_algorithm/binary_tree_traversal.ts
@@ -41,3 +41,52 @@ function inorderTraversal(root: TreeNode | null): number[] {
 //     if (!root) return [];
 //     return [...inorderTraversal(root.left), ...inorderTraversal(root.right), root.val];
 // };
+/////
+// 非递归实现中序遍历
+// function inorderTraversal(root: TreeNode | null): number[] {
+//   let res: number[] = [];
+//   let stack: TreeNode[] = [];
+//   let cur = root;
+//   while (cur || stack.length) {
+//     while (cur) {
+//       stack.push(cur);
+//       cur = cur.left;
+//     }
+//     cur = stack.pop();
+//     res.push(cur.val);
+//     cur = cur.right;
+//   }
+//   return res;
+// }
+// // 非递归实现前序遍历
+function prevTraversal(root: TreeNode | null): number[] {
+  const res: number[] = [];
+  const stack: TreeNode[] = [];
+  let cur = root;
+  while (cur || stack.length) {
+    while (cur) {
+      res.push(cur.val); // 前序遍历的区别就是这里
+      stack.push(cur);
+      cur = cur.left;
+    }
+    cur = stack.pop();
+    cur = cur.right;
+  }
+  return res;
+}
+// // 非递归实现后序遍历
+function backTraversal(root: TreeNode | null): number[] {
+  const res: number[] = [];
+  const stack: TreeNode[] = [];
+  let cur = root;
+  while (cur || stack.length) {
+    while (cur) {
+      res.unshift(cur.val); // 后序遍历的区别就是这里
+      stack.push(cur);
+      cur = cur.right;
+    }
+    cur = stack.pop();
+    cur = cur.left;
+  }
+  return res;
+}
